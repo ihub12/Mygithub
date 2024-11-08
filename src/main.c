40765260388c7789c67dea924ab549295517a427
SEO, JUNGHO <wjdgh9890@naver.com>
2024-11-08 22:22:16 +0900

    Initial commit - Add source files

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "hello.h"
+#include "world.h"
+
+int main(void){
+    print_hello();
+    print_world();
+}
---
+++ src/world.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "world.h"
+
+void print_world(){
+    printf("World\n");
+}
